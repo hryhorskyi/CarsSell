0c898e255259aee0d9805223026f84aa66b7de1d
Martin Hryhorskyi <92051937+hryhorskyi@users.noreply.github.com>
2021-12-14 18:14:32 +0200

    Update index.rb

--- index.rb
+++ index.rb
@@ -1,7 +1,6 @@
 require_relative 'requiries.rb'
 
 file_name = "#{File.dirname(__FILE__)}/db/cars.yml"
-file_searches = "#{File.dirname(__FILE__)}/db/searches.yml"
 
 cars = Management::DBReader.call(file_name)
 
@@ -11,10 +10,9 @@ searched_cars = Management::Search.call(cars, requests)
 
 sorted_cars = Management::Sort.call(searched_cars, requests)
 
-requests_up = requests.to_s
+count = Management::Statistic.call(requests)
 
-statistics = Management::Statistic.call(file_searches, requests_up, requests, sorted_cars)
+Management::SaveStat.call(sorted_cars, requests, count)
 
-Management::SaveStat.call(file_searches, statistics)
+Management::Output.call(sorted_cars, count)
 
-Management::Output.call(sorted_cars, statistics, requests_up)
